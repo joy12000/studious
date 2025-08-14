66674b40ee9c3fe710ddcdc381da6f94b07cc795
joy12000 <jeonguk1203@gmail.com>
2025-08-15 00:42:35 +0900

    chore: remove duplicate root files

--- sw.js
+++
@@ -1,4 +0,0 @@
-const VERSION = 'sw-v10';
-self.addEventListener('install', (e) => { self.skipWaiting(); });
-self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', () => {});
