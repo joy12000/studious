7f8afc69f75d14c59e0bed803cb6e9580c9837da
joy12000 <jeonguk1203@gmail.com>
2025-08-15 02:56:11 +0900

    Add files via upload

---
+++ src/Root.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import App from './App';
+import VersionBadge from './components/VersionBadge';
+export default function Root(){
+  return (<><App /><VersionBadge /></>);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
+import ErrorBoundary from './components/ErrorBoundary'
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { initInstallCapture } from './lib/install';
+import Root from './Root'
 
 initInstallCapture();
 
@@ -11,3 +13,11 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <Root />
+    </ErrorBoundary>
+  </React.StrictMode>
+)
