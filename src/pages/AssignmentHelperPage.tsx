9b254d03b89b764cb2a66e5e565bb284d3ded747
joy12000 <jeonguk1203@gmail.com>
2025-09-24 05:31:38 +0900

    fix: Apply dark mode to AssignmentHelperPage

--- src/pages/AssignmentHelperPage.tsx
+++ src/pages/AssignmentHelperPage.tsx
@@ -388,7 +388,7 @@ export default function AssignmentHelperPage() {
                     </div>
                 </div>
             )}
-            <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-50 p-4">
+            <div className="min-h-screen w-full flex flex-col items-center justify-center bg-background p-4">
                 <div className="w-full max-w-2xl">
                     {renderContent()}
                     <div className="flex justify-between mt-8">
