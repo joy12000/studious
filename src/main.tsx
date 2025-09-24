3d18205b001fdfe09d72cd2a55fd54b751376ec0
joy12000 <jeonguk1203@gmail.com>
2025-09-24 13:40:50 +0900

    fix(pdfjs): Set workerSrc to fix PDF rendering issues

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom/client'
 import ErrorBoundary from './components/ErrorBoundary'
 import Root from './App'
 import { initInstallCapture } from './lib/install'
+import * as pdfjsLib from 'pdfjs-dist';
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 initInstallCapture()
 
