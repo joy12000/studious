69fa2b80f25199d426f20eafd79b240fb85d9428
joy12000 <jeonguk1203@gmail.com>
2025-10-03 13:10:31 +0900

    feat(ui): Change page transition to scale animation

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -164,8 +164,8 @@ const AppLayout = ({ children, SignedIn, SignedOut, SignInButton, SignUpButton, 
                 <AnimatePresence mode="wait">
                   <motion.main
                     key={location.pathname}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
+                    initial={{ scale: 0.95, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
                     exit={{ opacity: 0 }}
                     transition={{ duration: 0.2 }}
                     className={`flex-1 flex flex-col p-4 pt-12 sm:p-6 ${
